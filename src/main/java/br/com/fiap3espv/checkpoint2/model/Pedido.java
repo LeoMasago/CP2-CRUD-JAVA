8058fa18700579efe61b9fa34142372e1855cbb9
Leo Kenzo Yamanaka Masago <leo.masago@happmobi.com.br>
2026-04-22 15:46:39 -0300

    Removendo validação feita em model/Pedido. A validação já está sendo feita em service

--- src/main/java/br/com/fiap3espv/checkpoint2/model/Pedido.java
+++ src/main/java/br/com/fiap3espv/checkpoint2/model/Pedido.java
@@ -1,8 +1,5 @@
 package br.com.fiap3espv.checkpoint2.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,12 +13,9 @@ public class Pedido {
     @Id
     private String id;
 
-    @NotBlank(message = "O nome do cliente é obrigatório")
     private String clienteNome;
 
     private LocalDate dataPedido = LocalDate.now();
 
-    @NotNull(message = "O valor total é obrigatório")
-    @PositiveOrZero(message = "O valor total não pode ser negativo")
     private Double valorTotal;
 }
